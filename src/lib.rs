486b1c2ca8f3fc396006eb6d6b7a09a0920dbc9f
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-30 23:31:54 -0300

    via trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod via_function;
+pub mod via_trait;
---
+++ src/via_trait.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+
+pub trait Handler {
+    const NAME: &'static str;
+
+    fn exec(self);
+}
+
+pub struct Request {
+    pub body: String,
+}
+
+pub trait FromRequest {
+    fn from_request(req: Request) -> Self;
+}
+
+pub trait Executor {
+    fn exec(&self, req: Request);
+}
+
+impl<F> Executor for F
+where
+    F: Fn(Request),
+{
+    fn exec(&self, req: Request) {
+        self(req)
+    }
+}
+
+trait DynExecutor {
+    fn call(&self, req: Request);
+}
+
+struct ErasedExecutor<E> {
+    executor: E,
+}
+
+impl<E> DynExecutor for ErasedExecutor<E>
+where
+    E: Executor,
+{
+    fn call(&self, req: Request) {
+        self.executor.exec(req);
+    }
+}
+
+#[derive(Default)]
+pub struct Router {
+    routes: HashMap<&'static str, Box<dyn DynExecutor>>,
+}
+
+impl Router {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn register<H>(mut self) -> Self
+    where
+        H: Handler + FromRequest + 'static,
+    {
+        self.routes.insert(
+            H::NAME,
+            Box::new(ErasedExecutor {
+                executor: |req| H::exec(H::from_request(req)),
+            }),
+        );
+        self
+    }
+
+    pub fn run(&self, pat: &'static str, req: Request) {
+        self.routes[pat].call(req);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+
+    use super::*;
+
+    #[test]
+    fn basic_handlers() {
+        static VEC: Mutex<Vec<i32>> = Mutex::new(Vec::new());
+
+        struct A;
+        impl FromRequest for A {
+            fn from_request(_req: Request) -> Self {
+                Self
+            }
+        }
+        impl Handler for A {
+            const NAME: &'static str = "a";
+
+            fn exec(self) {
+                VEC.lock().unwrap().push(1);
+            }
+        }
+
+        struct B(i32);
+        impl FromRequest for B {
+            fn from_request(req: Request) -> Self {
+                Self(req.body.parse().unwrap())
+            }
+        }
+        impl Handler for B {
+            const NAME: &'static str = "b";
+
+            fn exec(self) {
+                VEC.lock().unwrap().push(self.0);
+            }
+        }
+
+        let router = Router::new().register::<A>().register::<B>();
+
+        router.run("a", Request { body: "".into() });
+        router.run("b", Request { body: "2".into() });
+
+        let vec = VEC.lock().unwrap();
+        assert_eq!(*vec, &[1, 2]);
+    }
+}
